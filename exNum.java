599d681c3334adf25fdc58cf709f38ce69de5eae
1mNatthapong <152240605+1mNatthapong@users.noreply.github.com>
2024-07-25 14:31:40 +0700

    Create exNum.java

---
+++ exNum.java
@@ -0,0 +1,11 @@
+public class exNum {
+    public static void main(String[] args) {
+
+        int num1 = 62;
+        int num2 = 99;
+
+        int total = num1 + num2;
+
+        System.out.println("The sum of 62 and 99 is : "+total);
+    }
+}
