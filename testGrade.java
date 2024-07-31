5ec2edbc6e87966695e93f5907d6ce4bb421f34b
1mNatthapong <152240605+1mNatthapong@users.noreply.github.com>
2024-07-31 15:00:35 +0700

    Create testGrade.java

---
+++ testGrade.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class testGrade {
+    public static void main(String[] args) {
+        int score;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Your score : ");
+        score = in.nextInt();
+        if (score < 50) {
+            //Statement1
+            System.out.println("Your grade is F .");
+        }else if (score < 60) {
+            System.out.println("Your grade is D .");
+            //Statement1
+            
+        }else if (score < 70) {
+            System.out.println("Your grade is C .");
+        }else if (score < 80) {
+            System.out.println("Your grade is B .");
+        }else if (score < 100) {
+            System.out.println("Your grade is A .");
+        }else{
+           System.out.println("Tnvalid score"); //false
+        }
+    }
+}
