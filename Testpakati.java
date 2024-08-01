851e12df9a66ffeb438daabb4d5e0b79d72e5773
1mNatthapong <152240605+1mNatthapong@users.noreply.github.com>
2024-08-01 13:47:07 +0700

    Create Testpakati.java

---
+++ Testpakati.java
@@ -0,0 +1,14 @@
+switch (a) {
+    case 1:
+        x += 5;
+        break;
+    case 2:
+        x += 10;
+        break;
+    case 3:
+        x += 16;
+        break;
+    case 4:
+        x += 34;
+        break;
+}
